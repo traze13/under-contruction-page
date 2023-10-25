bf6fab8010b06194ed7162968dbca63499bab91c
Dzubayyan Ahmad <masdzub@gmail.com>
2023-10-25 22:24:37 +0700

    underconstruction

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const targetDate = new Date("2025-12-31 00:00:00").getTime();
+    const currentDate = new Date().getTime();
+    const timeRemaining = targetDate - currentDate;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
